2f2785d41d7aa1faa6fc32223ae83f0e2b038f54
Linkai Peng <72010396+lkpengcs@users.noreply.github.com>
2025-03-03 20:51:01 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-
+import netlify from '@astrojs/netlify';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pancreasdg.netlify.app/', // Replace with your GitHub Pages URL
-  base: '/pancreasdg/', // Replace with your actual repository name
+  site: 'https://pancreasdg.netlify.app/', 
+  base: '/', 
   output: 'static', // Ensures the build is static
   integrations: [tailwind(), icon(), mdx(), react()],
+  adapter: netlify(),
   markdown: {
     shikiConfig: {
       theme: "github-light"
